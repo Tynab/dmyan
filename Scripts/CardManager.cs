22f4e3493933f30bdebd74c9fe8fa613f5573c47
Yami An <yamiannephilim@gmail.com>
2025-06-16 00:11:52 +0700

    dm

--- Scripts/CardManager.cs
+++ Scripts/CardManager.cs
@@ -1,7 +1,6 @@
 using Godot;
 using Godot.Collections;
 using System.Linq;
-using static Godot.HttpRequest;
 using static Godot.MouseButton;
 using static System.Math;
 
@@ -9,138 +8,140 @@ namespace DMYAN.Scripts;
 
 public partial class CardManager : Node2D
 {
-	private const int CARD_COLLISION_MASK = 1;
+    private const int CARD_COLLISION_MASK = 1;
 
-	private Vector2 _screenSize;
-	private Card _selectedCard;
-	private bool _isHoveringCard;
+    private Vector2 _screenSize;
+    private Card _selectedCard;
+    private bool _isHoveringCard;
 
-	public override void _Ready() => _screenSize = GetViewportRect().Size;
+    public override void _Ready() => _screenSize = GetViewportRect().Size;
 
-	public override void _Input(InputEvent @event)
-	{
-		if (@event is InputEventMouseButton mouseEvent && mouseEvent.ButtonIndex is Left)
-		{
-			if (mouseEvent.Pressed)
-			{
-				var card = GetCardAtCursor();
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton mouseEvent && mouseEvent.ButtonIndex is Left)
+        {
+            if (mouseEvent.Pressed)
+            {
+                var card = GetCardAtCursor();
 
-				if (card is not null)
-				{
-					StartCardDrag(card);
-				}
-			}
-			else
-			{
-				FinishCardDrag();
-			}
-		}
-	}
+                if (card is not null)
+                {
+                    StartCardDrag(card);
+                }
+            }
+            else
+            {
+                StopCardDrag();
+            }
+        }
+    }
 
-	public override void _Process(double delta)
-	{
-		if (_selectedCard is not null)
-		{
-			var mousePosition = GetGlobalMousePosition();
+    public override void _Process(double delta)
+    {
+        if (_selectedCard is not null)
+        {
+            var mousePosition = GetGlobalMousePosition();
 
-			_selectedCard.Position = new Vector2(Clamp(mousePosition.X, 0, _screenSize.X), Clamp(mousePosition.Y, 0, _screenSize.Y));
-		}
-	}
+            _selectedCard.Position = new Vector2(Clamp(mousePosition.X, 0, _screenSize.X), Clamp(mousePosition.Y, 0, _screenSize.Y));
+        }
+    }
 
-	public void ConnectCardSignals(Card card)
-	{
-		if (card is null)
-		{
-			return;
-		}
+    public void ConnectCardSignals(Card card)
+    {
+        if (card is null)
+        {
+            return;
+        }
 
-		card.Hovered += OnCardHovered;
-		card.Unhovered += OnCardUnhovered;
-	}
+        card.Hovered += OnCardHovered;
+        card.Unhovered += OnCardUnhovered;
+    }
 
-	private void OnCardHovered(Card card)
-	{
-		if (card is null)
-		{
-			return;
-		}
+    private void OnCardHovered(Card card)
+    {
+        if (card is null)
+        {
+            return;
+        }
 
-		if (_isHoveringCard)
-		{
-			_isHoveringCard = false;
-			HighlightCard(card, true);
-		}
-	}
+        if (!_isHoveringCard)
+        {
+            _isHoveringCard = true;
+            HighlightCard(card, true);
+        }
+    }
 
-	private void OnCardUnhovered(Card card)
-	{
-		if (card is null)
-		{
-			return;
-		}
+    private void OnCardUnhovered(Card card)
+    {
+        if (card is null)
+        {
+            return;
+        }
 
-		HighlightCard(card, false);
+        if (_selectedCard is null)
+        {
+            HighlightCard(card, false);
 
-		var newCardHovered = GetCardAtCursor();
+            var newCardHovered = GetCardAtCursor();
 
-		if (newCardHovered is not null)// && newCardHovered != card)
-		{
-			//_isHoveringCard = true;
-			HighlightCard(newCardHovered, true);
-		}
-		else
-		{
-			_isHoveringCard = false;
-		}
-	}
+            if (newCardHovered is not null)
+            {
+                HighlightCard(newCardHovered, true);
+            }
+            else
+            {
+                _isHoveringCard = false;
+            }
+        }
+    }
 
-	private static void HighlightCard(Card card, bool hovered)
-	{
-		if (hovered)
-		{
-			card.Scale = new Vector2(1.05f, 1.05f);
-			card.ZIndex = 2;
-		}
-		else
-		{
-			card.Scale = Vector2.One;
-			card.ZIndex = 1;
-		}
-	}
+    private static void HighlightCard(Card card, bool hovered)
+    {
+        if (hovered)
+        {
+            card.Scale = new Vector2(1.05f, 1.05f);
+            card.ZIndex = 2;
+        }
+        else
+        {
+            card.Scale = Vector2.One;
+            card.ZIndex = 1;
+        }
+    }
 
-	private Card GetCardAtCursor()
-	{
-		var results = GetWorld2D().DirectSpaceState.IntersectPoint(new PhysicsPointQueryParameters2D
-		{
-			Position = GetGlobalMousePosition(),
-			CollideWithAreas = true,
-			CollisionMask = CARD_COLLISION_MASK
-		});
+    private Card GetCardAtCursor()
+    {
+        var results = GetWorld2D().DirectSpaceState.IntersectPoint(new PhysicsPointQueryParameters2D
+        {
+            Position = GetGlobalMousePosition(),
+            CollideWithAreas = true,
+            CollisionMask = CARD_COLLISION_MASK
+        });
 
-		return results.Count > 0 ? GetTopmostCard(results) : null;
-	}
+        return results.Count > 0 ? GetTopmostCard(results) : null;
+    }
 
-	private static Card GetTopmostCard(Array<Dictionary> cards) => cards.Select(static c => c["collider"].As<Area2D>().GetParent<Card>()).OrderByDescending(static c => c.ZIndex).FirstOrDefault();
+    private static Card GetTopmostCard(Array<Dictionary> cards) => cards.Select(static c => c["collider"].As<Area2D>().GetParent<Card>()).OrderByDescending(static c => c.ZIndex).FirstOrDefault();
 
-	private void StartCardDrag(Card card)
-	{
-		if (card is null)
-		{
-			return;
-		}
+    private void StartCardDrag(Card card)
+    {
+        if (card is null)
+        {
+            return;
+        }
 
-		_selectedCard = card;
-		card.Scale = Vector2.One;
-	}
+        _selectedCard = card;
+        card.Scale = Vector2.One;
+    }
 
-	private void FinishCardDrag()
-	{
-		if (_selectedCard is null)
-		{
-			return;
-		}
+    private void StopCardDrag()
+    {
+        if (_selectedCard is null)
+        {
+            return;
+        }
 
-		_selectedCard.Scale = new Vector2(1.05f, 1.05f);
-		_selectedCard = null;
-	}
+        _selectedCard.Scale = new Vector2(1.05f, 1.05f);
+        _selectedCard = null;
+    }
 }
