d7c9298caac2c59babede9af08ec6cbdd4adb10a
Yami An <yamiannephilim@gmail.com>
2025-06-15 23:58:41 +0700

    cc

---
+++ Scripts/Card.cs
@@ -0,0 +1,18 @@
+using Godot;
+
+namespace DMYAN.Scripts;
+
+public partial class Card : Node2D
+{
+    [Signal]
+    public delegate void HoveredEventHandler(Card card);
+
+    [Signal]
+    public delegate void UnhoveredEventHandler(Card card);
+
+    public override void _Ready() => GetParent().Call("ConnectCardSignals", this);
+
+    private void OnArea2DMouseEntered() => EmitSignal(SignalName.Hovered, this);
+
+    private void OnArea2DMouseExited() => EmitSignal(SignalName.Unhovered, this);
+}
