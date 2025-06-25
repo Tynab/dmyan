04b04dc0ae6a204c274a55c71c9505d1b1946ccd
Yami An <yamiannephilim@gmail.com>
2025-06-25 15:03:37 +0700

    303

--- Scripts/MainCardSlot.cs
+++ Scripts/MainCardSlot.cs
@@ -4,30 +4,30 @@ namespace DMYAN.Scripts;
 
 public partial class MainCardSlot : CardSlot
 {
-    [Export]
-    public PowerSlot PowerSlot { get; set; }
+	[Export]
+	public PowerSlot PowerSlot { get; set; }
 
-    public int CardsInSlot { get; set; } = 0;
+	public int CardsInSlot { get; set; } = 0;
 
-    public bool HasCardCanAttack { get; set; } = false;
+	public bool HasCardCanAttack { get; set; } = false;
 
-    public void SummonCard(Card card)
-    {
-        card.Summon(this);
-        HasCardInSlot = true;
-        CardsInSlot++;
-        PowerSlot.ShowPower(card, true);
-    }
+	public void SummonCard(Card card)
+	{
+		card.Summon(this);
+		HasCardInSlot = true;
+		CardsInSlot++;
+		PowerSlot.ShowPower(card, true);
+	}
 
-    public void SummonSetCard(Card card)
-    {
-        card.SummonSet(this);
-        HasCardInSlot = true;
-        CardsInSlot++;
+	public void SummonSetCard(Card card)
+	{
+		card.SummonSet(this);
+		HasCardInSlot = true;
+		CardsInSlot++;
 
-        if (DuelSide is DuelSide.Player)
-        {
-            PowerSlot.ShowPower(card, false);
-        }
-    }
+		if (DuelSide is DuelSide.Player)
+		{
+			PowerSlot.ShowPower(card, false);
+		}
+	}
 }
--- Scripts/MainZone.cs
+++ Scripts/MainZone.cs
@@ -6,44 +6,44 @@ namespace DMYAN.Scripts;
 
 public partial class MainZone : Node2D
 {
-    [Export]
-    public DuelSide DuelSide { get; set; } = DuelSide.None;
+	[Export]
+	public DuelSide DuelSide { get; set; } = DuelSide.None;
 
-    public int CardsInZone { get; set; } = 0;
+	public int CardsInZone { get; set; } = 0;
 
-    public bool HasCardCanAttack { get; set; } = false;
+	public bool HasCardCanAttack { get; set; } = false;
 
-    public void SummonCard(Card card)
-    {
-        GetMainSlot(true).SummonCard(card);
-        CardsInZone++;
-    }
+	public void SummonCard(Card card)
+	{
+		GetMainSlot(true).SummonCard(card);
+		CardsInZone++;
+	}
 
-    public void SummonSetCard(Card card)
-    {
-        GetMainSlot(false).SummonSetCard(card);
-        CardsInZone++;
-    }
+	public void SummonSetCard(Card card)
+	{
+		GetMainSlot(false).SummonSetCard(card);
+		CardsInZone++;
+	}
 
-    private MainCardSlot GetMainSlot(bool isAtk)
-    {
-        var emptySlots = new List<MainCardSlot>();
+	private MainCardSlot GetMainSlot(bool isAtk)
+	{
+		var emptySlots = new List<MainCardSlot>();
 
-        for (var i = 0; i < GetChildCount(); i++)
-        {
-            if (GetChild(i) is MainCardSlot currentSlot && !currentSlot.HasCardInSlot)
-            {
-                if (isAtk && i < 5)
-                {
-                    emptySlots.Add(currentSlot);
-                }
-                else if (i is > 4 and < 10)
-                {
-                    emptySlots.Add(currentSlot);
-                }
-            }
-        }
+		for (var i = 0; i < GetChildCount(); i++)
+		{
+			if (GetChild(i) is MainCardSlot currentSlot && !currentSlot.HasCardInSlot)
+			{
+				if (isAtk && i < 5)
+				{
+					emptySlots.Add(currentSlot);
+				}
+				else if (i is > 4 and < 10)
+				{
+					emptySlots.Add(currentSlot);
+				}
+			}
+		}
 
-        return emptySlots.Count > 0 ? emptySlots[RandRange(0, emptySlots.Count - 1)] : default;
-    }
+		return emptySlots.Count > 0 ? emptySlots[RandRange(0, emptySlots.Count - 1)] : default;
+	}
 }
