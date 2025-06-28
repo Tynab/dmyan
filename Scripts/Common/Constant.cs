a1d8e2683a5909bbb140c26e48e5f2ce1e2bd5d4
Yami An <yamiannephilim@gmail.com>
2025-06-28 23:39:45 +0700

    250628

--- Scripts/Common/Constant.cs
+++ Scripts/Common/Constant.cs
@@ -107,18 +107,6 @@ internal static class Constant
     internal const DuelSide STARTING_DUEL_SIDE = DuelSide.Player;
 }
 
-internal enum CardZone
-{
-    None = 0,         // Vô
-    MainDeck = 1,     // Bộ bài chính
-    ExtraDeck = 2,    // Bộ bài phụ
-    Main = 3,         // Chính khu
-    STP = 4,          // Phép trận
-    Field = 5,        // Dã khu
-    Graveyard = 6,    // Mộ địa
-    Banished = 7      // Loại trừ
-}
-
 internal enum CardFace
 {
     None = 0,         // Vô
---
+++ Scripts/Common/Enum/CardZone.cs
@@ -0,0 +1,13 @@
+namespace DMYAN.Scripts.Common.Enum;
+
+internal enum CardZone
+{
+    None = 0,         // Vô
+    MainDeck = 1,     // Bộ bài chính
+    ExtraDeck = 2,    // Bộ bài phụ
+    Main = 3,         // Chính khu
+    STP = 4,          // Phép trận
+    Field = 5,        // Dã khu
+    Graveyard = 6,    // Mộ địa
+    Banished = 7      // Loại trừ
+}
